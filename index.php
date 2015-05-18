a9432a6dc125357bc53d37e98e8381041d12cbbb
Julian van Riet <vanrietjulian@gmail.com>
2015-05-18 12:08:44 -0700

    Styles for Links

--- index.php
+++ index.php
@@ -6,25 +6,27 @@
 		<title>Julian van Riet</title>
 
 		<!-- links for stylsheets -->
-		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="css/styles.css">
 		<link rel="stylesheet" href="css/bootstrap-theme.css">
 		<link rel="stylesheet" href="css/bootstrap.css">
 
 	</head>
 	<body>
+
 		<!-- big jeader at top of page -->
 		<header>Julian van Riet</header>
 
 		<!-- links to different pages -->
-		<div>
-			<a href=""></a>
-			<a href=""></a>
-			<a href=""></a>
-			<a href=""></a>
+		<div class="links">
+			<div class="link"><a href="">Home</a></div>
+			<div class="link"><a href="">About Me</a></div>
+			<div class="link"><a href="">Projects/Work</a></div>
+			<div class="link"><a href="">Contacts/Social Media</a></div>
 		</div>
 		
-
+		<!-- js links -->
 		<script src="js/bootstrap.js"></script>
 		<script src="js/main.js"></script>
+
 	</body>
 </html>
