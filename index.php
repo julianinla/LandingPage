6b0218c9efbc77921891ba9701c24c78174474d4
Julian van Riet <vanrietjulian@gmail.com>
2015-05-18 11:52:29 -0700

    Basic Page

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<!-- title of my project -->
+		<title>Julian van Riet</title>
+
+		<!-- links for stylsheets -->
+		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="css/bootstrap-theme.css">
+		<link rel="stylesheet" href="css/bootstrap.css">
+
+	</head>
+	<body>
+		<!-- big jeader at top of page -->
+		<header>Julian van Riet</header>
+
+		<!-- links to different pages -->
+		<div>
+			<a href=""></a>
+			<a href=""></a>
+			<a href=""></a>
+			<a href=""></a>
+		</div>
+		
+
+		<script src="js/bootstrap.js"></script>
+		<script src="js/main.js"></script>
+	</body>
+</html>
