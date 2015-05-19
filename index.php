8ec9db8c24fc402ff1e1e9902c6ee2b1b5f3f6aa
Julian van Riet <vanrietjulian@gmail.com>
2015-05-19 10:57:53 -0700

    Completed Basic First Page

--- index.php
+++ index.php
@@ -18,10 +18,22 @@
 
 		<!-- links to different pages -->
 		<div class="links">
+			<!-- home link -->
 			<div class="link"><a href="">Home</a></div>
+			<!-- about me link -->
 			<div class="link"><a href="">About Me</a></div>
+			<!-- projects.work link -->
 			<div class="link"><a href="">Projects/Work</a></div>
+			<!-- contacts/social media link -->
 			<div class="link"><a href="">Contacts/Social Media</a></div>
+		</div>
+
+		<!-- main stuff -->
+		<div class="main-page">
+			<!-- home heading -->
+			<div class="heading1 link">Welcome</div>
+			<!-- main picture -->
+			<img src="img/sanfran.jpg" class="main-image">
 		</div>
 		
 		<!-- js links -->
